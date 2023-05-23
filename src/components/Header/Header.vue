0b3b710655f9ce9b04fb285d807be81e943fab54
truonggiang2612 <toidaumuonkhocprolaanhne@gmail.com>
2023-05-23 20:26:02 +0700

    first commit

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,51 @@
+<template>
+    <header class="header">
+        <div class="logo">
+            <img src="//src/assets/logo.png" alt="logo" />
+        </div>
+        <h1 class="title">{{ title }}</h1>
+        <div class="search">
+            <!-- Đặt component Search của bạn ở đây -->
+        </div>
+        <div class="time">
+            <!-- Đặt component Time của bạn ở đây -->
+        </div>
+    </header>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+    title: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<style scoped>
+.header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px;
+    background-color: #f2f2f2;
+}
+
+.logo {
+    /* CSS cho logo */
+}
+
+.title {
+    /* CSS cho title */
+}
+
+.search {
+    /* CSS cho search component */
+}
+
+.time {
+    /* CSS cho time component */
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        // Định nghĩa các route tại đây
+        // Ví dụ:
+        // { path: '/', component: Home },
+        // { path: '/about', component: About },
+    ]
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
